e2b96b4549c03b627e42991a7521f85b3bd90188
kabirt7 <kabir.t7@hotmail.com>
2024-08-21 13:26:37 +1000

    backend initialised

---
+++ resource-api-backend/src/main/java/io/nology/resource_api/ResourceApiApplication.java
@@ -0,0 +1,13 @@
+package io.nology.resource_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResourceApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResourceApiApplication.class, args);
+	}
+
+}
---
+++ resource-api-backend/src/test/java/io/nology/resource_api/ResourceApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.nology.resource_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResourceApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
